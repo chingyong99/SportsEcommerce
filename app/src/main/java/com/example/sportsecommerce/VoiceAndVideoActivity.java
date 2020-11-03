f7b967f4e2e0bd0d48fe5843cd8fcccb05e7e00c
siehchingyong <siehcy-wm17@student.tarc.edu.my>
2020-11-03 22:39:55 +0800

    initial

---
+++ app/src/main/java/com/example/sportsecommerce/VoiceAndVideoActivity.java
@@ -0,0 +1,14 @@
+package com.example.sportsecommerce;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VoiceAndVideoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_voice_and_video);
+    }
+}
